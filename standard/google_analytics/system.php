5f234e91f7809e6ff78b13541fcab4cd40af307b
Julius Jurgelenas <julius@jurgelenas.lt>
2013-02-05 22:07:03 +0200

    Initial commit

---
+++ standard/google_analytics/system.php
@@ -0,0 +1,23 @@
+<?php
+namespace Modules\standard\google_analytics;     
+
+if (!defined('CMS')) exit;
+
+class System{
+
+	public function init(){
+		global $site;
+
+		$tracking_code = strip_tags($this->get_option('ga_tracking_code'));
+
+		if((bool) $this->get_option('enable_ga'))
+			$site->addJavascriptContent('ga-tracking-code', $tracking_code);
+	}
+
+	private function get_option($key){
+		global $parametersMod;
+
+		return $parametersMod->getValue('standard', 'google_analytics', 'options', $key); 
+	}
+
+}
